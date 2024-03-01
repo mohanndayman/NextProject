545a4a334d2d82eb8de451d0c857b3b9ae432d11
mohanndayman <mohanndayman@yahoo.com>
2024-03-01 08:05:13 -0800

    S

--- app/components/SecondSecion.tsx
+++ app/components/SecondSecion.tsx
@@ -3,9 +3,16 @@ import React, { useEffect } from "react";
 import "../../styles/SecondSection.css"; // Import your CSS file
 import image from "../../images/100.jpg";
 import { PinContainer } from "./3d-pin";
+import image1 from "../../images/22.jpg";
+import image2 from "../../images/33.jpg";
+import { StaticImageData } from "next/image";
 
-// Define the AnimatedPinDemo component
-function AnimatedPinDemo() {
+interface AnimatedPinDemoProps {
+  title: string;
+  description: string;
+  image: StaticImageData;
+}
+function AnimatedPinDemo({ title, description, image }: AnimatedPinDemoProps) {
   useEffect(() => {
     // Add scroll event listener to trigger animation for all AnimatedPinDemo elements
     window.addEventListener("scroll", handleScroll);
@@ -28,19 +35,16 @@ function AnimatedPinDemo() {
 
   return (
     <div className="AnimatedPinDemo">
-      <PinContainer title="Test">
+      <PinContainer title={title}>
         <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem]">
           <h3
             className="max-w-xs !pb-2 !m-0 font-bold"
             style={{ color: "#ffc300" }}
           >
-            Product N2
+            {title}
           </h3>
           <div className="text-base !m-0 !p-0 font-normal">
-            <span style={{ color: "white" }}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Doloremque nemo eveniet commodi architecto nulla deleniti cum
-            </span>
+            <span style={{ color: "white" }}>{description}</span>
           </div>
           <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br" />
           <img src={image.src} alt="Description" />
@@ -58,7 +62,7 @@ function SecondSection() {
         <h1>Special Offers </h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam,
-          asperiores veniam.{" "}
+          asperiores veniam.
         </p>
         <hr
           style={{
@@ -70,13 +74,25 @@ function SecondSection() {
         />
       </div>
       <div className="Cards">
-        {/* Render three instances of AnimatedPinDemo */}
-        <AnimatedPinDemo />
-        <AnimatedPinDemo />
-        <AnimatedPinDemo />
+        {/* Render three instances of AnimatedPinDemo with different props */}
+        <AnimatedPinDemo
+          title="Product N1"
+          description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque nemo eveniet commodi architecto nulla deleniti cum"
+          image={image1}
+        />
+        <AnimatedPinDemo
+          title="Product N2"
+          description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque nemo eveniet commodi architecto nulla deleniti cum"
+          image={image}
+        />
+        <AnimatedPinDemo
+          title="Product N3"
+          description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque nemo eveniet commodi architecto nulla deleniti cum"
+          image={image2}
+        />
       </div>
     </div>
   );
 }
 
-export default SecondSection; // Export the SecondSection component
+export default SecondSection;
